3640b8c246612d8ddd4ab3ba1252259e9c8fe782
Alexander French <alexfrenchonthemove@gmail.com>
2021-04-01 12:07:20 +0200

    Created global variable defining function in main.py and updated test.py accordingly

--- main.py
+++ main.py
@@ -310,7 +310,7 @@ class Queen(Piece):
         self.steps = 7
 
 
-""" ------------------------------------------------------ IO ----------------------------------------------------------
+""" IO -----------------------------------------------------------------------------------------------------------------
 """
 
 
@@ -322,7 +322,7 @@ def get_player_move():
     return player_input
 
 
-""" ----------------------------------------------------- UTILS --------------------------------------------------
+""" UTILS --------------------------------------------------------------------------------------------------------------
 """
 
 
@@ -349,7 +349,101 @@ def flatten(l, ltypes=(list, tuple)):
     return ltype(l)
 
 
-""" ----------------------------------------------------- CONVERSIONS --------------------------------------------------
+""" GLOBAL IMPORT & VARIABLE DEFINITIONS -------------------------------------------------------------------------------
+"""
+
+
+def MAIN_VARIABLES():
+    """ All variables and imports for the main logic of the program
+        I separated this into a function to make tests.py function more cleanly
+    """
+    """ --------------------------- IMPORTS
+        """
+    global re
+
+    import re
+
+    """ --------------------------- MAIN VARIABLES
+    """
+    global VALUES, LETTERS, PATTERNS, STEPS, UNICODES, STARTING_ROWS, STANDARD_GAME
+    global KING_AND_PAWN_GAME, MINOR_GAME, MAJOR_GAME, FRONT_LINE, BACK_LINE, ROWS, FILES
+
+    VALUES = {
+        'Pawn': 1,
+        'Rook': 5,
+        'Knight': 3,
+        'Bishop': 3,
+        'King': 100,
+        'Queen': 9,
+    }
+
+    LETTERS = {
+        'P': 'Pawn',
+        'R': 'Rook',
+        'N': 'Knight',
+        'B': 'Bishop',
+        'K': 'King',
+        'Q': 'Queen',
+    }
+
+    PATTERNS = {
+        'Pawn': [(1, 1), (1, -1)],
+        'Rook': [(1, 0), (0, 1), (-1, 0), (0, -1)],
+        'Knight': [(1, 2), (2, 1), (-1, 2), (2, -1), (-2, 1), (1, -2), (-1, -2), (-2, -1)],
+        'Bishop': [(1, 1), (1, -1), (-1, 1), (-1, -1)],
+        'King': [(0, 1), (1, 0), (1, 1), (-1, 0), (0, -1), (-1, 1), (1, -1), (-1, -1)],
+        'Queen': [(0, 1), (1, 0), (1, 1), (-1, 0), (0, -1), (-1, 1), (1, -1), (-1, -1)],
+    }
+
+    STEPS = {
+        'Pawn': 1,
+        'Rook': 7,
+        'Knight': 1,
+        'Bishop': 7,
+        'King': 1,
+        'Queen': 7,
+    }
+
+    # The following have been designated THE WRONG WAY AROUND for the purposes of appearing more visually correct
+    # on a white-black-inverted terminal window
+    UNICODES = {
+        'Black': {
+            'Pawn': '♙',
+            'Rook': '♖',
+            'Knight': '♘',
+            'Bishop': '♗',
+            'King': '♔',
+            'Queen': '♕',
+        },
+        'White': {
+            'Pawn': '♟',
+            'Rook': '♜',
+            'Knight': '♞',
+            'Bishop': '♝',
+            'King': '♚',
+            'Queen': '♛',
+        },
+    }
+
+    # rows on which pieces start the game (the first and last two)
+    STARTING_ROWS = (7, 6, 1, 0)
+
+    # columns on which pieces start the game
+    STANDARD_GAME = (0, 1, 2, 3, 4, 5, 6, 7)  # all pieces
+    KING_AND_PAWN_GAME = (4,)  # only the king
+    MINOR_GAME = (1, 2, 4, 5, 6)  # minor pieces + king
+    MAJOR_GAME = (0, 3, 4, 7)  # major pieces + king
+
+    # pieces in the order they appear on the board
+    FRONT_LINE = ('Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn')
+    BACK_LINE = ('Rook', 'Knight', 'Bishop', 'Queen', 'King', 'Bishop', 'Knight', 'Rook')
+
+    # Ranks and Files
+    ROWS = (1, 2, 3, 4, 5, 6, 7, 8)
+    FILES = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+
+
+""" CONVERSIONS --------------------------------------------------------------------------------------------------------
 """
 
 
@@ -596,90 +690,10 @@ def tests():
 
 
 if __name__ == '__main__':
-    import re
-    import copy
-    from util import utils
-
-    """ --------------------------------------------- MAIN VARIABLES ---------------------------------------------------
-    """
-
-    VALUES = {
-        'Pawn': 1,
-        'Rook': 5,
-        'Knight': 3,
-        'Bishop': 3,
-        'King': 100,
-        'Queen': 9,
-    }
-
-    LETTERS = {
-        'P': 'Pawn',
-        'R': 'Rook',
-        'N': 'Knight',
-        'B': 'Bishop',
-        'K': 'King',
-        'Q': 'Queen',
-    }
-
-    PATTERNS = {
-        'Pawn': [(1, 1), (1, -1)],
-        'Rook': [(1, 0), (0, 1), (-1, 0), (0, -1)],
-        'Knight': [(1, 2), (2, 1), (-1, 2), (2, -1), (-2, 1), (1, -2), (-1, -2), (-2, -1)],
-        'Bishop': [(1, 1), (1, -1), (-1, 1), (-1, -1)],
-        'King': [(0, 1), (1, 0), (1, 1), (-1, 0), (0, -1), (-1, 1), (1, -1), (-1, -1)],
-        'Queen': [(0, 1), (1, 0), (1, 1), (-1, 0), (0, -1), (-1, 1), (1, -1), (-1, -1)],
-    }
-
-    STEPS = {
-        'Pawn': 1,
-        'Rook': 7,
-        'Knight': 1,
-        'Bishop': 7,
-        'King': 1,
-        'Queen': 7,
-    }
-
-    # The following have been designated THE WRONG WAY AROUND for the purposes of appearing more visually correct
-    # on a white-black-inverted terminal window
-    UNICODES = {
-        'Black': {
-            'Pawn': '♙',
-            'Rook': '♖',
-            'Knight': '♘',
-            'Bishop': '♗',
-            'King': '♔',
-            'Queen': '♕',
-        },
-        'White': {
-            'Pawn': '♟',
-            'Rook': '♜',
-            'Knight': '♞',
-            'Bishop': '♝',
-            'King': '♚',
-            'Queen': '♛',
-        },
-    }
-
-    # rows on which pieces start the game (the first and last two)
-    STARTING_ROWS = (7, 6, 1, 0)
-
-    # columns on which pieces start the game
-    STANDARD_GAME = (0, 1, 2, 3, 4, 5, 6, 7)  # all pieces
-    KING_AND_PAWN_GAME = (4,)  # only the king
-    MINOR_GAME = (1, 2, 4, 5, 6)  # minor pieces + king
-    MAJOR_GAME = (0, 3, 4, 7)  # major pieces + king
-
-    # pieces in the order they appear on the board
-    FRONT_LINE = ('Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn')
-    BACK_LINE = ('Rook', 'Knight', 'Bishop', 'Queen', 'King', 'Bishop', 'Knight', 'Rook')
-
-    # Ranks and Files
-    ROWS = (1, 2, 3, 4, 5, 6, 7, 8)
-    FILES = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
-
     """ ------------------------------------------------- MAIN BODY ----------------------------------------------------
     """
 
+    MAIN_VARIABLES()
     tests()
 
 
---
+++ tests.py
@@ -0,0 +1,16 @@
+# test.py
+""" This file houses the suite of tests for main.py classes and functions
+
+"""
+
+if __name__ == '__main__':
+    """ Primary imports and initialisation -----------------------------------------------------------------------------
+    """
+    import main
+    main.MAIN_VARIABLES()
+
+    """ Main -----------------------------------------------------------------------------------------------------------
+    """
+
+    board = main.Board()
+    print(board)
